b2d8015384f551e041ebf43995a0177cd00d1ac8
Rakibul Hasan Rajib <rajib@ap21.uc.osg-htc.org>
2024-10-16 10:47:46 -0500

    Saving

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -244,9 +244,7 @@ class LLaVATrainer(Trainer):
             weight_to_save = get_mm_adapter_state_maybe_zero_3(self.model.named_parameters(), keys_to_match)
 
             if self.args.local_rank == 0 or self.args.local_rank == -1:
-                print(output_dir)
-
-                self.model.config.save_pretrained(output_dir)
+                #self.model.config.save_pretrained(output_dir)
                 torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
         else:
             self.model.generation_config.do_sample = True
